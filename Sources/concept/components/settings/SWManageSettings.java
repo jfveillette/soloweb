bbd122a917184668ccd8669866df1faa08b5012d
hugith <hugi@karlmenn.is>
2014-03-25 11:02:29 +0000

    Cleanup in settings

--- Sources/concept/components/settings/SWManageSettings.java
+++ Sources/concept/components/settings/SWManageSettings.java
@@ -25,14 +25,15 @@ public class SWManageSettings extends SWAdminComponent {
 
 	public NSDictionary<String,String> tabDictionary() {
 		NSMutableDictionary<String,String> activeSettingsTabs = new NSMutableDictionary<>( Concept.sw().activeSettingsTabs() );
-		activeSettingsTabs.setObjectForKey( SWSettingsGeneral.class.getSimpleName(), SWLoc.string( "settingsTabGeneral", session() ) );
-		activeSettingsTabs.setObjectForKey( SWSettingsDatabase.class.getSimpleName(), SWLoc.string( "settingsTabDatabase", session() ) );
-		activeSettingsTabs.setObjectForKey( SWSettingsNews.class.getSimpleName(), SWLoc.string( "settingsTabNews", session() ) );
-		activeSettingsTabs.setObjectForKey( SWSettingsActions.class.getSimpleName(), SWLoc.string( "settingsTabAdministration", session() ) );
 		activeSettingsTabs.setObjectForKey( SWSettingsAccessControl.class.getSimpleName(), SWLoc.string( "settingsTabAccessControls", session() ) );
-		activeSettingsTabs.setObjectForKey( SWSettingsLocalization.class.getSimpleName(), SWLoc.string( "settingsTabLocalization", session() ) );
-		activeSettingsTabs.setObjectForKey( SWSettingsStatistics.class.getSimpleName(), SWLoc.string( "settingsTabStatistics", session() ) );
+		activeSettingsTabs.setObjectForKey( SWSettingsActions.class.getSimpleName(), SWLoc.string( "settingsTabAdministration", session() ) );
 		activeSettingsTabs.setObjectForKey( SWSettingsCustomInfo.class.getSimpleName(), "Custom info ofl" );
+		activeSettingsTabs.setObjectForKey( SWSettingsDatabase.class.getSimpleName(), SWLoc.string( "settingsTabDatabase", session() ) );
+		activeSettingsTabs.setObjectForKey( SWSettingsGeneral.class.getSimpleName(), SWLoc.string( "settingsTabGeneral", session() ) );
+		activeSettingsTabs.setObjectForKey( SWSettingsLocalization.class.getSimpleName(), SWLoc.string( "settingsTabLocalization", session() ) );
+		activeSettingsTabs.setObjectForKey( SWSettingsNews.class.getSimpleName(), SWLoc.string( "settingsTabNews", session() ) );
+		activeSettingsTabs.setObjectForKey( SWSettingsSites.class.getSimpleName(), "Vefir" );
+		activeSettingsTabs.setObjectForKey( SWSettingsStatistics.class.getSimpleName(), SWLoc.string( "settingsTabStatistics", session() ) );
 		return activeSettingsTabs;
 	}
 
