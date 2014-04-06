48ffd95a2069dd8cd21181d10652716f54e872da
hugith <hugi@karlmenn.is>
2014-04-06 14:31:38 +0000

    Filename cleanup

---
+++ Sources/concept/components/admin/sanity/SWSanityCheck.java
@@ -0,0 +1,12 @@
+package concept.components.admin.sanity;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.components.ERXComponent;
+
+public class SWSanityCheck extends ERXComponent {
+
+	public SWSanityCheck( WOContext context ) {
+		super( context );
+	}
+}
